76766bf13f3bca348d5aa6172a67b412bb6187fc
Paulo Oliveros <V8T2Z6L@johndeere.com>
2021-07-26 00:43:51 -0300

    fetching list from github API

--- 01-github-explorer/src/components/RepositoryItem.jsx
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem(props) {
     return (
         <li>
-            <strong>{props.repository?.name ?? 'Default'}</strong>
-            <p>{props.repository?.description}</p>
-            <a href={props.repository?.link}>
+            <strong>{props.repository.name}</strong>
+            <p>{props.repository.description}</p>
+            <a href={props.repository.http_url}>
                 Acessar repositório
             </a>
         </li>
--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,21 +1,24 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
-const repository = {
-    name: 'repo1',
-    description: 'Meu repositório',
-    link: 'https://github.com/pahique/ignite'
-}
-
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/pahique/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data));
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>;
+                })}
             </ul>
 
         </section>
