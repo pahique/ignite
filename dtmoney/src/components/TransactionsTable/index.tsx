088dce3cc0b1006183dc4ad3bb06f6f1a1786dec
Paulo Oliveros <V8T2Z6L@johndeere.com>
2021-08-04 00:21:58 -0300

    Transactions Table

---
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,44 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>01/08/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$1.000</td>
+                        <td>Casa</td>
+                        <td>05/08/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Condomínio</td>
+                        <td className="withdraw">- R$300</td>
+                        <td>Casa</td>
+                        <td>10/08/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Empréstimo</td>
+                        <td className="deposit">R$10.000</td>
+                        <td>Finanças</td>
+                        <td>04/08/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
+}
