87424deea73508891592db4099df9ee092310b24
Paulo Oliveros <V8T2Z6L@johndeere.com>
2021-08-04 23:02:47 -0300

    Adding MirageJS and Axios

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get('/transactions', () => {
+      return [{
+        id: 1,
+        title: "Transaction 1",
+        amount: 400,
+        type: 'deposit',
+        category: 'Food',
+        createAt: new Date()
+      }, {
+        id: 2,
+        title: "Transaction 2",
+        amount: -200,
+        type: 'withdraw',
+        category: 'Credit Card',
+        createAt: new Date()
+      }];
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
