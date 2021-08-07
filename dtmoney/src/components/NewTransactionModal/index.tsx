8093a0eb717c07e90f80be7def4efb857226dfd2
Paulo Oliveros <V8T2Z6L@johndeere.com>
2021-08-06 22:39:56 -0300

    RadioBox

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -8,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+
     return (
         <Modal isOpen={isOpen}
             onRequestClose={onRequestClose}
@@ -26,6 +31,18 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <h2>Cadastrar transação</h2>
                 <input placeholder="Título" />
                 <input type="number" placeholder="Valor" />
+                <TransactionTypeContainer>
+                    <RadioBox type="button" isActive={type === 'deposit'} 
+                            onClick={() => setType('deposit')}>
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+                    <RadioBox type="button" isActive={type === 'withdraw'} 
+                            onClick={() => setType('withdraw')}>
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
                 <input placeholder="Categoria"/>
                 <button type="submit">
                     Cadastrar
--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -39,7 +40,7 @@ export const Container = styled.form`
         font-size: 1rem;
         margin-top: 1.5rem;
         font-weight: 600;
-        
+
         transition: filter 0.2s;
 
         &:hover {
@@ -47,3 +48,43 @@ export const Container = styled.form`
         }
     }
 `;
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5rem;
+`;
+
+interface RadioBoxProps {
+    isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+    background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+    
+    &:hover {
+        border-color: ${darken(0.1, '#d7d7d7')};
+    }
+
+    img {
+        height: 20px;
+        width: 20px;
+    }
+
+    span {
+        display: inline-block;
+        margin-left: 1rem;
+        font-size: 1rem;
+        color: var(--text-title)
+    }
+`;
+
