e5500d5cd5cc04bdad081f479c0e8db5d3f48706
Paulo Oliveros <V8T2Z6L@johndeere.com>
2021-08-25 23:15:08 -0300

    Home page

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,15 +1,25 @@
 import Head from 'next/head';
-import styles from '../styles/home.module.scss';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
-        Hello <span>World</span>
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <p>👏 Hey, welcome</p>
+          <h1>New about the <span>React</span> world</h1>
+          <p>
+            Get access to all the applications<br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
