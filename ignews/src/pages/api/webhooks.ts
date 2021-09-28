3a3adee71550d83c2e3f9ff6ef656e3a3607f2ed
Paulo Oliveros <V8T2Z6L@johndeere.com>
2021-09-28 00:12:07 -0300

    Subscriptions

--- ignews/src/pages/api/webhooks.ts
+++ ignews/src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Readable } from  'stream';
 import Stripe from 'stripe';
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscriptions';
 
 // codigo pronto
 async function buffer(readable: Readable) {
@@ -21,7 +22,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.updated',
+    'customer.subscription.deleted'
 ]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -37,7 +40,32 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         }
         const { type } = event;
         if (relevantEvents.has(type)) {
-            console.log('evento recebido', event);
+            try {
+                switch(type) {
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+                        const subscription = event.data.object as Stripe.Subscription;
+                        await saveSubscription(
+                            subscription.id, 
+                            subscription.customer.toString(),
+                            false
+                        )
+                        break;
+                    case 'checkout.session.completed':
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString(),
+                            true
+                        );
+                        break;
+                    default: 
+                        throw new Error('Unhandled event: ' + type);
+                }
+            } catch(err) {
+                // sentry, bugsnag, etc
+                return res.json({error: 'Webhook handler failed'})
+            }
         }
 
         res.status(200).json({ received: true});
